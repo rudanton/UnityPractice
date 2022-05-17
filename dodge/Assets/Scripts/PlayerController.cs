223bd5b661b02962a1c85b15071a67732b901ea5
rudolphen <rlaejrgh159@gmail.com>
2022-05-17 18:23:03 +0900

    shortenCode

--- dodge/Assets/Scripts/PlayerController.cs
+++ dodge/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     Rigidbody rg;
     public float vConst;
     Material myMaterial;
+    WaitForSeconds wait;
     // Start is called before the first frame update
     void Start()
     {
         rg = GetComponent<Rigidbody>();
         myMaterial = GetComponent<MeshRenderer>().materials[0];
+        wait = new WaitForSeconds(0.1f);
     }
 
     // Update is called once per frame
@@ -29,14 +31,12 @@ public class PlayerController : MonoBehaviour
         }
     }
     IEnumerator flash(){
-        myMaterial.color = Color.red;
-        yield return new WaitForSeconds(0.1f);
-        myMaterial.color = Color.white;
-        yield return new WaitForSeconds(0.1f);
-        myMaterial.color = Color.red;
-        yield return new WaitForSeconds(0.1f);
-        myMaterial.color = Color.white;
-        yield return new WaitForSeconds(0.1f);
-        
+        for(int i = 0 ; i<8 ; i++)
+        {
+            // if (myMaterial.color == Color.white) myMaterial.color = Color.red;
+            // else myMaterial.color = Color.white;
+            myMaterial.color = myMaterial.color == Color.white ? Color.red : Color.white ; 
+            yield return wait;
+        }
     }
 }
